8dbb1017928d4213fafd3cacd2d8fc6b49882cdc
Henok Ayenew <henacodes@gmail.com>
2024-02-03 12:06:07 +0400

    change tabs

--- src/lib/TabList.svelte
+++ src/lib/TabList.svelte
@@ -1,22 +1,40 @@
 <script>
 	import { PlusCircle, PencilLine, Password } from 'phosphor-svelte';
+	import dashboardStore from '../stores/dashboardStore';
+
+	const returnTabClass = (tabName) => {
+		if (tabName === $dashboardStore.openTab) {
+			return;
+		}
+		return '';
+	};
+
+	const selectedTabClass = 'bg-[#2b3440] text-slate-400';
+	const changeTab = (tabName) => {
+		dashboardStore.update((curr) => {
+			return {
+				...curr,
+				openTab: tabName
+			};
+		});
+	};
 </script>
 
 <ul class="menu bg-base-100 w-56 rounded-box">
-	<li class="p-3">
-		<a>
+	<li class="p-3" on:click={() => changeTab('newProduct')}>
+		<a class={$dashboardStore.openTab === 'newProduct' ? selectedTabClass : ''}>
 			<PlusCircle />
 			New Product
 		</a>
 	</li>
-	<li class="p-3">
-		<a>
+	<li class="p-3" on:click={() => changeTab('editProduct')}>
+		<a class={$dashboardStore.openTab === 'editProduct' ? selectedTabClass : ''}>
 			<PencilLine />
 			Edit Product
 		</a>
 	</li>
-	<li class="p-3">
-		<a>
+	<li class="p-3" on:click={() => changeTab('changePassword')}>
+		<a class={$dashboardStore.openTab === 'changePassword' ? selectedTabClass : ''}>
 			<Password />
 			Change Password
 		</a>
--- src/stores/dashboardStore.js
+++ src/stores/dashboardStore.js
@@ -1,1 +1,5 @@
 import { writable } from 'svelte/store';
+
+export default writable({
+	openTab: 'newProduct'
+});

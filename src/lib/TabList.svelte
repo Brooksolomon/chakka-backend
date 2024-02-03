1b65ccd15fa60716ba92e612722a0d11289cfd43
Henok Ayenew <henacodes@gmail.com>
2024-02-03 11:17:36 +0400

    added the addtional pages

---
+++ src/lib/TabList.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { PlusCircle, PencilLine, Password } from 'phosphor-svelte';
+</script>
+
+<ul class="menu bg-base-100 w-56 rounded-box">
+	<li class="p-3">
+		<a>
+			<PlusCircle />
+			New Product
+		</a>
+	</li>
+	<li class="p-3">
+		<a>
+			<PencilLine />
+			Edit Product
+		</a>
+	</li>
+	<li class="p-3">
+		<a>
+			<Password />
+			Change Password
+		</a>
+	</li>
+</ul>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import TabList from '../../lib/TabList.svelte';
+</script>
+
+<div class=" h-[100%] flex">
+	<div class="dashboard-left flex-[0.1]">
+		<TabList />
+	</div>
+	<div class="dashboard-right flex-[0.9]"></div>
+</div>

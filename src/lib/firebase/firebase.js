ddeaa8029139039f1bd1e918ad27ad32d841aaa0
Brook Solomon <officialbrooksolo@gmail.com>
2024-02-03 20:31:36 +0300

    fix

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getStorage, ref, uploadBytes} from 'firebase/storage'
+import {getStorage, ref, uploadBytes,listAll} from 'firebase/storage'
 import {v4} from 'uuid';
 
 import { getAnalytics } from "firebase/analytics";
@@ -41,6 +41,9 @@ async function addProduct(productID,name,source,price,description,category,image
     });
 
 }
+//addProduct(3,"EARTH BLOOM","Rosemary",21.95,"Rosmarinus officinalis 10 ML 100% PURE ESSENTIAL OIL, STEAM DISTILLED  Eastern Afromontane Biodiversity Hotspot Hair and fragrance oil Leveraging its potent antiseptic and antibacterial properties, Rosemary is a versatile solution for hair treatments, reducing topical inflammation, and promoting gut detoxification.  Properties: Eucalyptol, Terpineol, D-Limonene, Linalool, Humulene","Cold-Pressed Oils",["https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Earth_Bloom_3_280x420.jpg?v=1695822453","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Earth_Bloom_1_280x420.jpg?v=1695822453","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Earth_Bloom_2_280x420.jpg?v=1695822453","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Earth_Bloom_4_280x420.jpg?v=1695651672"])
+addProduct(1,"SWEET BOTANY","Lemongrass",17.95,"Embrace Lemongrass, an aromatic ally with antioxidant and anti-septic wonders. Experience its inflammation-reducing charm as it disinfects and enlivens the skin, becoming an essential ingredient in fragrance and skincare routines.Properties: Citral, Geraniol, Limonene, Elemol, Citronellal","Essential-Oils",["https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Sweet_Botany_2_280x420.jpg?v=1695822510","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Sweet_Botany_1_280x420.jpg?v=1695822510","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Sweet_Botany_3_280x420.jpg?v=1695652064"])
+addProduct(2,"SPICY HERBAL","Ethiopian Cardamom",21.95,"Aframomum corrorima 10 ML 100% PURE ESSENTIAL OIL, STEAM DISTILLED Radiant skin, captivating scent, and clear breathing Embrace Ethiopian Cardamom's inflammation-reducing, antiseptic, and anti-bacterial properties, elevating a  skincare routine, crafting alluring fragrances, and providing soothing relief during congestion. Properties:Linalool, Pinene, Sabinene, Myrcene, Limonene, 1,8-Cineole","Herbs And Spices",["https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Spicy_Herbal_2_280x420.jpg?v=1695822503","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Spicy_Herbal_2_280x420.jpg?v=1695822503","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Spicy_Herbal_3_280x420.jpg?v=1695651735","https://cdn.shopify.com/s/files/1/0819/8881/3081/files/Spicy_Herbal_4_280x420.jpg?v=1695651735"])
 
 
 
@@ -120,13 +123,19 @@ async function addToPending(txnReference,firstName,lastName,email,city,subCity,d
 }
 async function addImage(productID,img)
 {
-  const imageRef = ref(storage,String(productID)+'/'+img.name +v4());
+  const imageRef = ref(storage,String(productID)+'/'+"img" +v4());
   const res = await uploadBytes(imageRef,img);
   console.log(res);
   console.log("uploaded")
 
 }
-
+async function fetchImageForProuct(productID)
+{
+  const imageListRef = ref(storage,String(productID)+'/')
+  const urls = await listAll(imageListRef)
+  console.log(urls) 
+}
+fetchImageForProuct(1)
 
 export const FireFunc = {
   addProduct:addProduct ,//void

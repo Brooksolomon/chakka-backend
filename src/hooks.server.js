4ca3292248c52134741cc473a4df9f98035dfa5b
Brook Solomon <officialbrooksolo@gmail.com>
2024-02-03 08:56:40 +0300

    rah

--- src/hooks.server.js
+++
@@ -1,9 +0,0 @@
-import {FireFunc} from './lib/firebase/firebase'
-export async function handle({ event, resolve }) {
-	if(event.url.pathname.startsWith('/confirmMoney')){
-		FireFunc.addProduct("LOG")
-		return new Response("diick")
-	}else{
-		return resolve(event)
-	}
-}

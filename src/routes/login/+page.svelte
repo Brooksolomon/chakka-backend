a81a1b1b6d24e68c04660bd9787edf9a56539a6c
Brook Solomon <officialbrooksolo@gmail.com>
2024-02-04 16:42:00 +0300

    minor change

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -14,7 +14,6 @@
 		}
 		const loggedIn = await loginFunction(username, password);
 		if (loggedIn){
-		sessionStorage.setItem("loggedin",true)
 		goto('/dashboard')
 		loginStore.set(true)
 		wrong = false
